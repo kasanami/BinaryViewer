5564ef8d22d616d1bcf4d3c6d6bbb5110a071075
笠浪貴大 <dev.kacha@gmail.com>
2019-04-28 01:51:07 +0900

    基本機能実装

--- BinaryViewer/Program.cs
+++ BinaryViewer/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
+using System.IO;
 
 namespace BinaryViewer
 {
@@ -9,6 +8,42 @@ namespace BinaryViewer
     {
         static void Main(string[] args)
         {
+#if DEBUG
+            // デバッグ用に読み込むファイル
+            args = new string[] { "../../Program.cs" };
+#endif
+            if (args.Length < 1)
+            {
+                Console.WriteLine("BinaryViewer");
+                Console.WriteLine("第1引数に開くファイルのパスを指定する。");
+                return;
+            }
+            var path = args[0];
+            try
+            {
+                var fileStream = File.OpenRead(path);
+                Console.WriteLine(path + "を正常に開けた。");
+
+                while (true)
+                {
+                    var bytes = new byte[16];// 0で初期化も兼ねている
+                    var readBytes = fileStream.Read(bytes, 0, bytes.Length);
+                    for (int i = 0; i < readBytes; i++)
+                    {
+                        Console.Write(bytes[i].ToString("X2") + " ");
+                    }
+                    var s = Encoding.UTF8.GetString(bytes).Replace('\n', ' ').Replace('\r', ' ');
+                    Console.WriteLine(s);
+                    if (readBytes < bytes.Length)
+                    {
+                        break;
+                    }
+                }
+            }
+            catch
+            {
+
+            }
         }
     }
 }
